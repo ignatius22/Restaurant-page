ebd09294a4826d056bde47b9ce7f161725ce6800
Ignatius22 <igee54077@gmail.com>
2020-09-18 03:39:43 +0100

    modify contact page

--- src/contact.js
+++ src/contact.js
@@ -1,15 +1,34 @@
 const content = document.getElementById('content');
 
 const contact = `
-<div class="container p-2 mt-3">
-    <h1 class="border-bottom">Contact Us</h1>
-    <h4>Reach out to us today and have us amaze you</h4>
-  <p class="lead">
-  <address>
-  <strong>Phone/WhatsApp:</strong> +234 706 369 8007<br>
-  <strong>Instagram:</strong> @exquisitekitchen 
-  </address>
-  </p>
+<div class="container contact-form">
+            <div class="contact-image">
+                <img src="https://image.ibb.co/kUagtU/rocket_contact.png" alt="rocket_contact"/>
+            </div>
+            <form method="post">
+                <h3>Drop Us a Message</h3>
+               <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <input type="text" name="txtName" class="form-control" placeholder="Your Name *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="txtEmail" class="form-control" placeholder="Your Email *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="txtPhone" class="form-control" placeholder="Your Phone Number *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="btnSubmit" class="btnContact" value="Send Message" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <textarea name="txtMsg" class="form-control" placeholder="Your Message *" style="width: 100%; height: 150px;"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </form>
 </div>
 `;
 
