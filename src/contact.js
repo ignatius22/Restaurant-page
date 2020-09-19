05c2f9b73941c17dbf3a96989058b22873632409
Ignatius22 <igee54077@gmail.com>
2020-09-19 09:23:18 +0100

    modify contact js

--- src/contact.js
+++ src/contact.js
@@ -20,7 +20,7 @@ const loadContact = () => {
   const contactIcons = document.createElement('div');
   contactIcons.setAttribute('id', 'contact-icons');
   contactIcons.className = 'text-white';
-  contactIcons.innerHTML = `
+  contactIcons.textContent = `
     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content.
 
     Company: HostRiver
